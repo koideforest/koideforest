b9bbc35f788647f1f2099c5c5fe4bdc8b97bcf94
koideforest <koideforest@gmail.com>
2019-11-25 19:10:21 +0900

    Add p-band example

--- tb.py
+++ tb.py
@@ -1,7 +1,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from operator import itemgetter
 
-class ReciprocalVector():
+class ReciprocalVector_band():
 
     def __init__( self, start = [], label = '' ):
         if start == []:
@@ -9,26 +10,26 @@ class ReciprocalVector():
         else:
             self.k = [ np.array( start ) ]
         if label == '':
-            self.label     = []
-            self.labeltick = []
+            self.ticklabel = []
             self.tick      = []
+            self.axis      = []
         else:
-            self.label     = [ label ]
-            self.labeltick = [ 0 ]
+            self.ticklabel = [ label ]
             self.tick      = [ 0 ]
+            self.axis      = [ 0 ]
         self.mesh = 100
 
     def initial_k_point( self, start, label = '' ):
         self.k.append( start )
-        self.label.append( label )
-        try:
-            self.labeltick.append( self.labeltick[-1] + 1 )
-        except IndexError:
-            self.labeltick.append( 0 )
+        self.ticklabel.append( label )
         try:
             self.tick.append( self.tick[-1] + 1 )
         except IndexError:
             self.tick.append( 0 )
+        try:
+            self.axis.append( self.axis[-1] + 1 )
+        except IndexError:
+            self.axis.append( 0 )
 
     def interpolate_k( self, start, end, n ):
         interval = ( np.array( end ) - np.array( start ) ) / n
@@ -42,21 +43,21 @@ class ReciprocalVector():
         if n == -1:
             n = self.mesh
         self.k += self.interpolate_k( self.k[ -1 ], end, n )
-        self.label.append( label )
+        self.ticklabel.append( label )
         try:
-            self.labeltick.append( self.labeltick[-1] + n )
+            self.tick.append( self.tick[-1] + n )
         except IndexError:
-            self.labeltick.append( n )
+            self.tick.append( n )
         try:
-            self.tick += [ self.tick[-1] + i + 1 for i in range( n ) ]
+            self.axis += [ self.axis[-1] + i + 1 for i in range( n ) ]
         except IndexError:
-            self.tick += [ i + 1 for i in range( n ) ]
+            self.axis += [ i + 1 for i in range( n ) ]
 
     def reset( self ):
         self.k         = [] 
-        self.label     = []
-        self.labeltick = []
+        self.ticklabel = []
         self.tick      = []
+        self.axis      = []
 
 def NeighborSC1( a ):
     R = []
@@ -104,27 +105,122 @@ def SlaterKoster_ss( R, sigma ):
         T.append( sigma )
     return T
 
+def SlaterKoster_sp( R, sigma, xyz = 'x' ):
+    # c = cos\theta = e_xyz \cdot ( R - 0 ) / ( |e_xyz| | R - 0 | )
+    epsilon = 1e-7
+    T = []
+    for r_ in R:
+        if xyz == 'x':
+            c = r_[0]
+        elif xyz == 'y':
+            c = r_[1]
+        else:  # xyz == 'z'
+            c = r_[2]
+        c /= np.linalg.norm( r_ ) + epsilon
+        T.append( c * sigma )
+    return T
+
+def SlaterKoster_ps( R, sigma, xyz = 'x' ):
+    return SlaterKoster_sp( [ -r_ for r_ in R ], sigma, xyz )
+
+def SlaterKoster_pp( R, sigma, pi, xyz1 = 'x', xyz2 = 'x' ):
+    epsilon = 1e-7
+    T = []
+    if not xyz1 in ( 'x', 'y', 'z' ):
+        return T
+    if not xyz2 in ( 'x', 'y', 'z' ):
+        return T
+    for r_ in R:
+        if xyz1 == xyz2:
+            if xyz1 == 'x':
+                c = r_[0]
+            elif xyz1 == 'y':
+                c = r_[1]
+            else:  # xyz1 == xyz2 == 'z'
+                c = r_[2]
+            c /= np.linalg.norm( r_ ) + epsilon
+            T.append( c**2 * sigma +( 1 - c**2 ) * pi )
+        else:
+            if xyz1 == 'x':
+                c1 = r_[0]
+                if xyz2 == 'y':
+                    c2 = r_[1]
+                else:  # xyz2 == 'z'
+                    c2 = r_[2]
+            elif xyz2 == 'y':
+                c1 = r_[1]
+                if xyz2 == 'z':
+                    c2 = r_[2]
+                else:  # xyz2 == 'x'
+                    c2 = r_[0]
+            else:  # xyz1 == 'z'
+                c1 = r_[2]
+                if xyz2 == 'x':
+                    c2 = r_[0]
+                else:  # xyz2 == 'y'
+                    c2 = r_[1]
+            c = c1 * c2 / ( np.linalg.norm( r_ ) + epsilon )**2
+            T.append( c * ( sigma - pi )  )
+    return T
+
+
 if __name__ == '__main__':
 
     a = 2
     R = NeighborFCC1( a )
-    T = SlaterKoster_ss( sigma )
-    
+
     #vec_k = ReciprocalVector( list( np.pi/a * np.array( [ 1, 1, 1 ] ) ), 'L' )
-    vec_k = ReciprocalVector( )
+    vec_k = ReciprocalVector_band( )
     vec_k.initial_k_point( list( np.pi/a * np.array( [ 1, 1, 1 ] ) ), 'L' )
     vec_k.add_k_point( [ 0 ] * 3, '$\Gamma$' ) 
     vec_k.add_k_point( [ 2 * np.pi / a, 0, 0  ], 'X' )
-    
-    h0 = -1
-    E = []
+ 
+    # s band
+    h0    = -1
+    sigma = -0.2
+    T     = SlaterKoster_ss( R, sigma )
+    E     = []
     for k_ in vec_k.k:
         E_ = [ np. exp( 1j * np.dot( k_, r_ ) ) * t_ for r_, t_ in zip( R, T ) ]
         E.append( h0 + sum( E_ ).real )
     
     fig = plt.figure()
     ax  = fig.add_subplot( 1, 1, 1 )
-    ax.plot( vec_k.tick, E )
-    ax.set_xticks( vec_k.labeltick )
-    ax.set_xticklabels( vec_k.label )
+    ax.plot( vec_k.axis, E )
+    ax.set_xticks( vec_k.tick )
+    ax.set_xticklabels( vec_k.ticklabel )
     plt.show()
+
+    # p band
+    h0    = -1
+    sigma = 0.2
+    pi    = -0.02
+    E, U  = [], []
+    xyz   = ( 'x', 'y', 'z' )
+
+    for k_ in vec_k.k:
+        H = np.zeros( ( 3, 3 ), dtype = np.complex )
+        for i1 in range( 3 ):
+            for i2 in range( 3 ):
+                if i1 == i2:
+                    H[ i1 ][ i2 ] += h0
+                T = SlaterKoster_pp( R, sigma, pi, xyz[i1], xyz[i2] )
+                H[ i1 ][ i2 ] += sum( [ np.exp( 1j * np.dot( k_, r_ ) ) * t_ for r_, t_ in zip( R, T ) ] )
+        value, vector = np.linalg.eigh( np.real( H ) )
+        eigen = sorted( [ [ val, vec ] for val, vec in zip( value, vector ) ], key = itemgetter( 0 ) )
+        E.append( [ eig[0] for eig in eigen ] )
+        U.append( [ eig[1] for eig in eigen ] )
+
+    E = np.array( E )
+    U = np.array( U )
+
+    fig    = plt.figure()
+    ax     = fig.add_subplot( 1, 1, 1 )
+    colors = ( 'r', 'g', 'b' )
+    for e_, u_ in zip( E.T, U.T ):
+        for iu, u__ in enumerate( u_ ):
+            ax.scatter( vec_k.axis, e_, s = abs( u__ )**2 * 50, c = colors[ iu ], lw = 0 )
+    ax.set_xticks( vec_k.tick )
+    ax.set_xticklabels( vec_k.ticklabel )
+    plt.show()
+
