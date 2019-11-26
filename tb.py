c96c5a9f99c77e29d5eae5bd89a3d4513bdfd203
koideforest <koideforest@gmail.com>
2019-11-26 19:46:30 +0900

    TranslationVector_Neighbor was added

--- tb.py
+++ tb.py
@@ -1,3 +1,4 @@
+import itertools
 import numpy as np
 from matplotlib import pyplot as plt
 from operator import itemgetter
@@ -59,7 +60,8 @@ class ReciprocalVector_band():
         self.tick      = []
         self.axis      = []
 
-def NeighborSC1( a ):
+
+def Neighbor1_SC( a ):
     R = []
     pm = ( -1, 1 )
     for i in pm:
@@ -68,7 +70,7 @@ def NeighborSC1( a ):
         R.append( a * np.array( [ 0, 0, i ] ) )
     return R
 
-def NeighborSC2( a ):
+def Neighbor2_SC( a ):
     R = []
     pm = ( -1, 1 )
     for i1 in pm:
@@ -78,7 +80,7 @@ def NeighborSC2( a ):
             R.append( a * np.array( [ i2, 0, i1 ] ) )
     return R
 
-def NeighborSC3( a ):
+def Neighbor3_SC( a ):
     R = []
     pm = ( -1, 1 )
     for i1 in pm:
@@ -87,87 +89,191 @@ def NeighborSC3( a ):
             R.append( a * np.array( i1, i2, i3 ) )
     return R
 
-def NeighborBCC1( a ):
-    return NeighborSC3( a/2 )
+def Neighbor1_BCC( a ):
+    return Neighbor3_SC( a/2 )
 
-def NeighborBCC2( a ):
-    return NeighborSC1( a )
+def Neighbor2_BCC( a ):
+    return Neighbor1_SC( a )
 
-def NeighborFCC1( a ):
-    return NeighborSC2( a/2 )
+def Neighbor1_FCC( a ):
+    return Neighbor2_SC( a/2 )
 
-def NeighborFCC2( a ):
-    return NeighborSC1( a )
+def Neighbor2_FCC( a ):
+    return Neighbor1_SC( a )
 
-def SlaterKoster_ss( R, sigma ):
-    T = []
-    for r_ in R:
-        T.append( sigma )
-    return T
+def Neighbor1_Graphene( LC ):
+    # Translational vector
+    t1 = LC * np.array( [  1/2, np.sqrt(3)/2, 0 ] )
+    t2 = LC * np.array( [ -1/2, np.sqrt(3)/2, 0 ] )
+    T  = ( t1, t2 )
 
-def SlaterKoster_sp( R, sigma, xyz = 'x' ):
+    # atomic site in a unit cell 
+    zero = np.zeros(3)
+    dn   = LC/np.sqrt(3)
+    a1   = zero
+    a2   = dn * np.array( [ 0, 1, 0 ] )
+    A    = ( a1, a2 )
+
+    #pmz = ( -1, 0, 1 )
+    #R_ = []
+    #for i1 in pmz:
+    #    for i2 in pmz:
+    #        R_.append( a1 + i1 * t1 + i2 * t2 )
+    #        R_.append( a2 + i1 * t1 + i2 * t2 )
+    #R = []
+    #for a_ in A:
+    #    R.append( [ r__ for r__ in R_ if ( np.linalg.norm( r__ - a_ ) <= dn ) and any( r__ - a_ != zero ) ] )
+    #return R
+
+    return Neighbor( T, 1, A, dn )
+
+def Neighbor1_Graphene_0( LC ):
+    # Translational vector
+    t1 = LC * np.array( [  1/2, np.sqrt(3)/2, 0 ] )
+    t2 = LC * np.array( [ -1/2, np.sqrt(3)/2, 0 ] )
+    
+    # atomic site in a unit cell 
+    zero = np.zeros(3)
+    dn   = LC/np.sqrt(3)
+    a1   = zero
+    a2   = dn * np.array( [ 0, 1, 0 ] )
+    A    = ( a1, a2 )
+
+    pmz = ( -1, 0, 1 )
+    R_ = []
+    for i1 in pmz:
+        for i2 in pmz:
+            R_.append( a1 + i1 * t1 + i2 * t2 )
+            R_.append( a2 + i1 * t1 + i2 * t2 )
+
+    R = []
+    for a_ in A:
+        R.append( [ r__ for r__ in R_ if ( np.linalg.norm( r__ - a_ ) <= dn ) and any( r__ - a_ != zero ) ] )
+    return R
+
+
+def Neighbor( T, nt, A, dn ):
+    R_  = []
+    # for example ( -1, 0, 1 ) -> ( -1, -1 ), ( -1, 0 ), ( -1, 1 )...
+    #ijk = itertools.product( np.arange( -nt, nt + 1 ), repeat = len( T ) )
+
+    for a_ in A:
+        ijk = itertools.product( np.arange( -nt, nt + 1 ), repeat = len( T ) )
+        for i_ in ijk:
+            R_.append( np.array( a_ ) + sum( [ i_[it] * np.array( t_ ) for it, t_ in enumerate( T ) ] ) )
+
+    zero = np.zeros( len( T[0] ) ) 
+    R    = []
+    for a_ in A:
+        R.append( [ r__ for r__ in R_ if ( np.linalg.norm( r__ - a_ ) <= dn ) and any( r__ - a_ != zero ) ] )
+    return R
+
+def TranslationVector_Neighbor( r0, r1, T, nt, dn ):
+    ijk  = itertools.product( np.arange( -nt, nt + 1 ), repeat = len( T ) )
+    zero = np.zeros( len( T[0] ) )
+
+    TVN = []
+    for i_ in ijk:
+        t__ = sum( [ i_[ it ] * np.array( t_ ) for it, t_ in enumerate( T ) ] )
+        r_  = np.array( r1 ) + t__
+        if ( np.linalg.norm( r_ - np.array( r0 ) ) <= dn ) and any( r_ - np.array( r0 ) != zero ):
+            TVN.append( t__ )
+    return TVN
+
+
+def SlaterKoster_ss( r, sigma ):
+    return sigma
+
+def SlaterKoster_sp( r, sigma, xyz = 'x' ):
     # c = cos\theta = e_xyz \cdot ( R - 0 ) / ( |e_xyz| | R - 0 | )
     epsilon = 1e-7
-    T = []
-    for r_ in R:
-        if xyz == 'x':
-            c = r_[0]
-        elif xyz == 'y':
-            c = r_[1]
-        else:  # xyz == 'z'
-            c = r_[2]
-        c /= np.linalg.norm( r_ ) + epsilon
-        T.append( c * sigma )
-    return T
+    if xyz == 'x':
+        c = r[0]
+    elif xyz == 'y':
+        c = r[1]
+    else:  # xyz == 'z'
+        c = r[2]
+    c /= np.linalg.norm( r ) + epsilon
+    return c * sigma
 
-def SlaterKoster_ps( R, sigma, xyz = 'x' ):
-    return SlaterKoster_sp( [ -r_ for r_ in R ], sigma, xyz )
+def SlaterKoster_ps( r, sigma, xyz = 'x' ):
+    return SlaterKoster_sp( -r, sigma, xyz )
 
-def SlaterKoster_pp( R, sigma, pi, xyz1 = 'x', xyz2 = 'x' ):
+def SlaterKoster_pp( r, sigma, pi, xyz1 = 'x', xyz2 = 'x' ):
     epsilon = 1e-7
-    T = []
     if not xyz1 in ( 'x', 'y', 'z' ):
-        return T
+        return False
     if not xyz2 in ( 'x', 'y', 'z' ):
-        return T
-    for r_ in R:
-        if xyz1 == xyz2:
-            if xyz1 == 'x':
-                c = r_[0]
-            elif xyz1 == 'y':
-                c = r_[1]
-            else:  # xyz1 == xyz2 == 'z'
-                c = r_[2]
-            c /= np.linalg.norm( r_ ) + epsilon
-            T.append( c**2 * sigma +( 1 - c**2 ) * pi )
-        else:
-            if xyz1 == 'x':
-                c1 = r_[0]
-                if xyz2 == 'y':
-                    c2 = r_[1]
-                else:  # xyz2 == 'z'
-                    c2 = r_[2]
-            elif xyz2 == 'y':
-                c1 = r_[1]
-                if xyz2 == 'z':
-                    c2 = r_[2]
-                else:  # xyz2 == 'x'
-                    c2 = r_[0]
-            else:  # xyz1 == 'z'
-                c1 = r_[2]
-                if xyz2 == 'x':
-                    c2 = r_[0]
-                else:  # xyz2 == 'y'
-                    c2 = r_[1]
-            c = c1 * c2 / ( np.linalg.norm( r_ ) + epsilon )**2
-            T.append( c * ( sigma - pi )  )
-    return T
+        return False
+    if xyz1 == xyz2:
+        if xyz1 == 'x':
+            c = r[0]
+        elif xyz1 == 'y':
+            c = r[1]
+        else:  # xyz1 == xyz2 == 'z'
+            c = r[2]
+        c /= np.linalg.norm( r ) + epsilon
+        return c**2 * sigma + ( 1 - c**2 ) * pi
+    else:
+        if xyz1 == 'x':
+            c1 = r[0]
+            if xyz2 == 'y':
+                c2 = r[1]
+            else:  # xyz2 == 'z'
+                c2 = r[2]
+        elif xyz2 == 'y':
+            c1 = r[1]
+            if xyz2 == 'z':
+                c2 = r[2]
+            else:  # xyz2 == 'x'
+                c2 = r[0]
+        else:  # xyz1 == 'z'
+            c1 = r[2]
+            if xyz2 == 'x':
+                c2 = r[0]
+            else:  # xyz2 == 'y'
+                c2 = r[1]
+        c = c1 * c2 / ( np.linalg.norm( r ) + epsilon )**2
+        return c * ( sigma - pi )
+
+def SlaterKoster( r, V, l1, l2 ):
+    # V = ( sigma, pi, delta )
+    L = { l1, l2 }
+    if   L == { 's', 's' }:
+        return SlaterKoster_ss( r, V[0] )
+    elif L == { 's', 'px' }:
+        r_ = r
+        if l1 == 'px':
+            r_ = -r
+        return SlaterKoster_sp( r_, V[0], 'x' )
+    elif L == { 's', 'py' }:
+        r_ = r
+        if l1 == 'py':
+            r_ = -r
+        return SlaterKoster_sp( r_, V[0], 'y' )
+    elif L == { 's', 'px' }:
+        r_ = r
+        if l1 == 'pz':
+            r_ = -r
+        return SlaterKoster_sp( r_, V[0], 'z' )
+    elif L == { 'px', 'px' }:
+        return SlaterKoster_pp( r, V[0], V[1], 'x', 'x' )
+    elif L == { 'py', 'py' }:
+        return SlaterKoster_pp( r, V[0], V[1], 'y', 'y' )
+    elif L == { 'pz', 'pz' }:
+        return SlaterKoster_pp( r, V[0], V[1], 'z', 'z' )
+    elif L == { 'px', 'py' }:
+        return SlaterKoster_pp( r, V[0], V[1], 'x', 'y' )
+    elif L == { 'py', 'pz' }:
+        return SlaterKoster_pp( r, V[0], V[1], 'y', 'z' )
+    elif L == { 'pz', 'px' }:
+        return SlaterKoster_pp( r, V[0], V[1], 'z', 'x' )
 
 
 if __name__ == '__main__':
 
     a = 2
-    R = NeighborFCC1( a )
+    R = Neighbor1_FCC( a )
 
     #vec_k = ReciprocalVector( list( np.pi/a * np.array( [ 1, 1, 1 ] ) ), 'L' )
     vec_k = ReciprocalVector_band( )
@@ -181,7 +287,7 @@ if __name__ == '__main__':
     T     = SlaterKoster_ss( R, sigma )
     E     = []
     for k_ in vec_k.k:
-        E_ = [ np. exp( 1j * np.dot( k_, r_ ) ) * t_ for r_, t_ in zip( R, T ) ]
+        E_ = [ np. exp( 1j * np.dot( k_, r_ ) ) * SlaterKoster_ss( r_, sigma ) for r_ in R ]
         E.append( h0 + sum( E_ ).real )
     
     fig = plt.figure()
@@ -204,8 +310,8 @@ if __name__ == '__main__':
             for i2 in range( 3 ):
                 if i1 == i2:
                     H[ i1 ][ i2 ] += h0
-                T = SlaterKoster_pp( R, sigma, pi, xyz[i1], xyz[i2] )
-                H[ i1 ][ i2 ] += sum( [ np.exp( 1j * np.dot( k_, r_ ) ) * t_ for r_, t_ in zip( R, T ) ] )
+                H[ i1 ][ i2 ] += sum( [   np.exp( 1j * np.dot( k_, r_ ) ) 
+                                        * SlaterKoster_pp( r_, sigma, pi, xyz[i1], xyz[i2] ) for r_ in R ] )
         value, vector = np.linalg.eigh( np.real( H ) )
         eigen = sorted( [ [ val, vec ] for val, vec in zip( value, vector ) ], key = itemgetter( 0 ) )
         E.append( [ eig[0] for eig in eigen ] )
@@ -217,7 +323,7 @@ if __name__ == '__main__':
     fig    = plt.figure()
     ax     = fig.add_subplot( 1, 1, 1 )
     colors = ( 'r', 'g', 'b' )
-    for e_, u_ in zip( E.T, U.T ):
+    for e_, u_ in zip( E.T, U.transpose( 1, 2, 0 ) ):  # E[k][n], U[k][n][i]
         for iu, u__ in enumerate( u_ ):
             ax.scatter( vec_k.axis, e_, s = abs( u__ )**2 * 50, c = colors[ iu ], lw = 0 )
     ax.set_xticks( vec_k.tick )
